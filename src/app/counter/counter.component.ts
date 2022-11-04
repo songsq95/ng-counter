a7a87740f6f7e4a4042ce761b2fd0f69e8aa640b
Siqi Song <ssong6@slb.com>
2022-11-04 13:38:00 +0800

    feat: add plus button

---
+++ src/app/counter/counter.component.ts
@@ -0,0 +1,18 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-counter',
+  templateUrl: './counter.component.html',
+  styleUrls: ['./counter.component.scss']
+})
+export class CounterComponent implements OnInit {
+  count: number = 0;
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+  increase(): void{
+    this.count++;
+  }
+
+}
